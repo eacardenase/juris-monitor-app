95b041f0e376ba976e7f71d1757d751ea09cdf4e
Edwin Cardenas <eacardenase@gmail.com>
2024-07-09 20:17:49 -0500

    feat: set root route based on user authentication state

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
-  get 'pages/landing_page'
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
+
+  authenticated(:user) do
+    root "pages#index", as: :authenticated_root
+  end
+
+  unauthenticated(:user) do
+    root 'pages#landing_page'
+  end
+
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  root "pages#index"
 end
